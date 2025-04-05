53a888f7571d2ce73adcf88d32b2425a5616d073
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-05 01:49:42 -0600

    solved #49 - Group Anagrams

---
+++ Arrays & Hashing/49-group-anagrams.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n • k)
+// Space Complexity: O(n • k)
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+
+        for (string& str : strs) {
+            vector<int> freq(26);
+            for (char c : str) {
+                freq[c - 'a']++;
+            }
+
+            string freqStr;
+            for (int cnt : freq) {
+                freqStr.push_back(cnt);
+                freqStr.push_back('/'); 
+            }
+
+            map[freqStr].push_back(str); 
+        }
+
+        vector<vector<string>> ans;
+        for (auto& [key, val] : map) {
+            ans.push_back(val);
+        }
+
+        return ans;
+    }
+};
