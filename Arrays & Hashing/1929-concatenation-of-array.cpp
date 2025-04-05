a37ed6eb92e9c5f81253fe9736a1d2065812300e
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-05 12:28:52 -0600

    solved #27 - Remove Element

--- Arrays & Hashing/1929-concatenation-of-array.cpp
+++ Arrays & Hashing/1929-concatenation-of-array.cpp
@@ -8,8 +8,8 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         vector<int> ans;
-        ans.insert(ans.begin(), nums.begin(), nums.end());
-        ans.insert(ans.begin() + nums.size(), nums.begin(), nums.end());
+        ans.insert(ans.end(), nums.begin(), nums.end());
+        ans.insert(ans.end(), nums.begin(), nums.end());
 
         return ans;
     }
---
+++ Arrays & Hashing/27-remove-element.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
