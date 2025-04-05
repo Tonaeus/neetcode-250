87afb60f66ffc23facef4f3a6b32b0d91c9956d6
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-05 16:19:46 -0600

    solved #75 - Sort Colors

---
+++ Arrays & Hashing/75-sort-colors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Algorithm: Dutch National Flag
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        int k = nums.size() - 1;
+
+        while (j <= k) {
+            if (nums[j] == 0) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+            else if (nums[j] == 1) {
+                j++;
+            }
+            else {
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+
+        return;
+    }
+};
