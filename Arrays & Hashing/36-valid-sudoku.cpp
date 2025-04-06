b7f32ac6c008c8536ec5fe309ac734e902af3bd8
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-06 00:44:36 -0600

    solved #36 - Valid Sudoku

---
+++ Arrays & Hashing/36-valid-sudoku.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        array<unordered_set<char>, 9> rows; 
+        array<unordered_set<char>, 9> cols; 
+        array<unordered_set<char>, 9> boxes; 
+        
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    continue;
+                }
+
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r].contains(board[r][c]) or 
+                    cols[c].contains(board[r][c]) or 
+                    boxes[boxIndex].contains(board[r][c])) {
+                    return false;
+                }
+
+                rows[r].insert(board[r][c]);
+                cols[c].insert(board[r][c]);
+                boxes[boxIndex].insert(board[r][c]);
+            }
+        }
+
+        return true;
+    }
+};
