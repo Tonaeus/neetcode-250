049ae1e29e8b3042acd9f17a0cfdf955c516d059
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-11 00:08:26 -0600

    solved #41 - First Missing Positive

---
+++ Arrays & Hashing/41-first-missing-positive.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Algorithm: Cycle Sort
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (0 < nums[i] and nums[i] <= n and nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
