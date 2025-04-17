1d6f28aee34b0fdef1053a1f959e3b16afd62941
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-16 21:35:24 -0600

    solved #881 - Boats to Save People

---
+++ Two Pointers/881-boats-to-save-people.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n•log(n))
+// Space Complexity: O(log(n))
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans = 0;
+        int l = 0;
+        int r = people.size() - 1;
+
+        sort(people.begin(), people.end());
+
+        while (l <= r) {
+            if (l != r and people[l] + people[r] <= limit) {
+                ans++;
+                l++;
+                r--;
+            }
+            else {
+                ans++;
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};
