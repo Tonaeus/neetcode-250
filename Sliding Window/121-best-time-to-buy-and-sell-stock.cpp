f7dc1addfeae103375325a9b9629316cdd4a5395
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-18 12:11:14 -0600

    solved #121 - Best Time to Buy and Sell Stock

---
+++ Sliding Window/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int minBuy = prices[0];
+
+        for(int i = 1; i < prices.size(); i++) {
+            ans = max(ans, prices[i] - minBuy);
+            minBuy = min(minBuy, prices[i]);
+        }
+
+        return ans;
+    }
+};
