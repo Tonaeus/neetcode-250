f6e5347f4008895b1eea1ad330f93cdefa388315
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-05-08 21:16:40 -0600

    solved #153 - Find Minimum in Rotated Sorted Array

---
+++ Binary Search/153-find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] <= nums[r]) {
+                r = mid;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+
+        return nums[l];
+    }
+};
