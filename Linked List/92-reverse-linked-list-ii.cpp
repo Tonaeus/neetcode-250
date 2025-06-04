fecee3b4088d0b303ff3c41ba0b12302a9ec8f23
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-06-03 22:14:32 -0600

    solved #92 - Reverse Linked List II

---
+++ Linked List/92-reverse-linked-list-ii.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode dummy(0, head);
+        ListNode* bl = &dummy; 
+
+        for (int i = 0; i < left - 1; i++) {
+            bl = bl->next; 
+        }
+
+        ListNode* r = bl->next;
+        ListNode* curr = nullptr;
+
+        for (int i = 0; i < right - left; i++) {
+            curr = r->next;
+            r->next = curr->next;
+            curr->next = bl->next;
+            bl->next = curr;
+        }
+
+        return dummy.next;
+    }
+};
