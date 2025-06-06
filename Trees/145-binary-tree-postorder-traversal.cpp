31073cb970c119df71c8a562f8b9ac72db6788ec
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-06-05 19:05:56 -0600

    solved #145 - Binary Tree Postorder Traversal

---
+++ Trees/145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: 
+// Space Complexity: 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+
+private:
+    void postorder(TreeNode* node, vector<int>& ans) {
+        if (!node) {
+            return; 
+        }
+
+        postorder(node->left, ans);
+        postorder(node->right, ans);
+        ans.push_back(node->val);
+
+        return;
+    }
+};
