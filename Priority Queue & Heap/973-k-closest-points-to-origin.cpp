e59ebc2039c5e1c3a8b1957d5e471969d0cc0fc0
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-06-17 22:16:34 -0600

    solved #973 - K Closest Points to Origin

---
+++ Priority Queue & Heap/973-k-closest-points-to-origin.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(k)
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto dist = [](const auto& p) { 
+            return (p[0] * p[0]) + (p[1] * p[1]);
+        };
+        auto cmp = [&](const auto& a, const auto& b) { 
+            return dist(a) < dist(b);
+        };
+        
+        nth_element(points.begin(), points.begin() + k - 1, points.end(), cmp);
+
+        return vector(points.begin(), points.begin() + k);
+    }
+};
