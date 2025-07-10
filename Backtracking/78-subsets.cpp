d2c87759b415a3946190a2cd47405070d4e44653
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-07-09 21:22:20 -0600

    solved #78 - Subsets

---
+++ Backtracking/78-subsets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Technique: Bitmasking
+
+// Time Complexity: O(2^n * n)
+// Space Complexity: O(2^n)
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < (1 << n); i++) {
+            vector<int> subset;
+            for (int j = 0; j < n; j++) {
+                if (i & (1 << j)) {
+                    subset.push_back(nums[j]);
+                }
+            }
+            ans.push_back(subset);
+        }
+
+        return ans;
+    }
+};
