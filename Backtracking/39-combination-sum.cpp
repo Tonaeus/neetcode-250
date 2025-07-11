d886c4dfc38ebf9b4a9f03bc9af8b7d7da9c0295
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-07-10 20:50:59 -0600

    solved #39 - Combination Sum

---
+++ Backtracking/39-combination-sum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(2^n * n)
+// Space Complexity: O(2^n * n)
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        dfs(candidates, 0, target, ans, comb);
+        return ans;
+    }
+
+private:
+    void dfs(vector<int>& candidates, int i, int target, vector<vector<int>>& ans, vector<int>& comb) {
+        if (target == 0) {
+            ans.push_back(comb);
+            return;
+        }
+
+        for (int j = i; j < candidates.size(); j++) {
+            if (target - candidates[j] >= 0) {
+                comb.push_back(candidates[j]);
+                dfs(candidates, j, target - candidates[j], ans, comb);
+                comb.pop_back();
+            }
+        }
+
+        return;
+    }
+};
