29a2c27f74fcacb1c1604f2280f46412c8c6e2f5
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-06 22:44:13 -0600

    solved #695 - Max Area of Island

---
+++ Graphs/695-max-area-of-island.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Algorithm: Flood Fill
+
+// Time Complexity: O(r * c)
+// Space Complexity: O(r * c)
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+
+        int ans = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 1) {
+                    ans = max(ans, dfs(grid, r, c, i, j));
+                }
+            }
+        }
+
+        return ans;
+    }
+
+private:
+    int dfs(vector<vector<int>>& grid, int r, int c, int i, int j) {
+        if (i < 0 or i >= r or j < 0 or j >= c or grid[i][j] == 0) {
+            return 0;
+        }
+
+        grid[i][j] = 0;
+
+        int res = 0;
+
+        static const int directions[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for (const auto& [dr, dc] : directions) {
+            res += dfs(grid, r, c, i + dr, j + dc);
+        }
+
+        return res + 1;
+    }
+};
