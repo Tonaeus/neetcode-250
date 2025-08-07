f06881dfe01f9a83b33e3d3ab374ac7885f36b0f
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-08-06 22:35:17 -0600

    solved #953 - Verifying an Alien Dictionary

---
+++ Graphs/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Structure: Map
+
+// Time Complexity: O(n * k), n = number of words, k = average word length
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        vector<int> map(26);
+
+        for (int i = 0; i < 26; i++) {
+            map[order[i] - 'a'] = i;
+        }
+        
+        for (int i = 1; i < words.size(); i++) {
+            string& w1 = words[i - 1];
+            string& w2 = words[i];
+
+            for (int j = 0; j < w1.length(); j++) {
+                if (j == w2.length()) {
+                    return false;
+                }
+                if (w1[j] != w2[j]) {
+                    if (map[w1[j] - 'a'] > map[w2[j] - 'a']) {
+                        return false;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return true;
+    }
+};
