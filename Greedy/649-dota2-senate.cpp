550143b496272fdc53cab698af0ef769bfe52ae9
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2026-04-29 16:10:38 -0600

    solved #649 - Dota2 Senate

--- Greedy/649-dota2-senate.cpp
+++ Greedy/649-dota2-senate.cpp
@@ -1,12 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Time Complexity: 
-// Space Complexity: 
+// Data Structure: Queue
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        
+        int n = senate.size();
+        queue<int> rq, dq;
+
+        for (int i = 0; i < n; i++) {
+            senate[i] == 'R' ? rq.push(i) : dq.push(i);
+        }
+
+        while (!rq.empty() and !dq.empty()) {
+            int ri = rq.front();
+            rq.pop();
+            
+            int di = dq.front();
+            dq.pop();
+
+            if (ri < di) {
+                rq.push(ri + n);
+            }
+            else {
+                dq.push(di + n);
+            }
+        }
+
+        return rq.empty() ? "Dire" : "Radiant";
     }
 };
