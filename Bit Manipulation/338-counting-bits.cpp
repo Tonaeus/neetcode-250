588148c29d9413cf4d42952443394d38f0dbf202
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2026-05-07 09:54:52 -0600

    solved #338 - Counting Bits

---
+++ Bit Manipulation/338-counting-bits.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Technique: Dynamic Programming
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1);
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+
+        return dp;
+    }
+};
--- Stacks/84-largest-rectangle-in-histogram.cpp
+++ Stacks/84-largest-rectangle-in-histogram.cpp
@@ -18,17 +18,17 @@ public:
             int start = i;
             while (!ims.empty() and ims.top().second > heights[i]) {
                 auto [index, height] = ims.top();
+                ims.pop();
                 ans = max(ans, height * (i - index));
                 start = index;
-                ims.pop();
             }
             ims.push({start, heights[i]});
         }
 
         while (!ims.empty()) {
             auto [index, height] = ims.top();
-            ans = max(ans, height * (n - index));
             ims.pop();
+            ans = max(ans, height * (n - index));
         }
 
         return ans;
